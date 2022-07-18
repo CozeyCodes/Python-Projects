6e23420bbd6a2ce0424718706a31372bc9336513
Cozey <99023466+CozeyCodes@users.noreply.github.com>
2022-07-18 15:51:02 +0545

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,16 @@
+
+
+quiz = {
+    1 : {
+        'question' : "Who is the first person to step on moon?",
+        'answer' : "Neil Armstrong"
+    },
+    2 : {
+        'question' : "Which is the most visited website of all time?",
+        'answer' : "Google"
+    },
+    3 : {
+        'question' : "What is the full form of IBM?",
+        'answer' : "International Business Machines"
+    }
+}
